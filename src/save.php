82bbfe86762682303b8e5b397b4b8bbb963b5314
Siyu Kat Wang <siyukw@gmail.com>
2017-02-21 16:16:20 -0800

    I'm dying this is hard

--- src/save.php
+++ src/save.php
@@ -2,5 +2,11 @@
     $receivedName = $_POST['sendingName'];
     $receivedId = $_POST['sendingId'];
 
+    $file = "attendance.txt";
 
+    $content = $receivedId . ": " . $receiveName . "\n";
+
+    file_put_contents($file, $content, FILE_APPEND);
+
+    echo "Successsssss";
 ?>
